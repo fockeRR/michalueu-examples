e65083624644a0e0c815b60b6e4844d633140d85
Michał Urbaniak <urbaniakm.we@gmail.com>
2014-02-17 20:00:17 +0100

    Add comments

--- webview_js/src/eu/michalu/WebViewActivity.java
+++ webview_js/src/eu/michalu/WebViewActivity.java
@@ -1,15 +1,15 @@
 package eu.michalu;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.net.http.SslError;
 import android.os.Bundle;
-import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class WebViewActivity extends Activity {
 	WebView mWebView;
+	/*
+	 * array of div's ids to disable/hide
+	 */
 	String[] idsToHide = { "section1", "section3", "section5" };
 
 	@Override
@@ -19,40 +19,22 @@ public class WebViewActivity extends Activity {
 		setContentView(R.layout.main);
 
 		mWebView = (WebView) findViewById(R.id.activity_webview);
+		//enable javascript engine
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.setWebViewClient(new WebViewClient() {
-
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				view.loadUrl(url);
-				return true;
-			}
-
-			@Override
-			public void onPageStarted(WebView view, String url, Bitmap favicon) {
-				// TODO Auto-generated method stub
-				super.onPageStarted(view, url, favicon);
-
-			}
-
 			@Override
 			public void onPageFinished(WebView view, String url) {
 				// TODO Auto-generated method stub
 				super.onPageFinished(view, url);
+				
+				//run 'disableSection' for all divs to hide/disable
 				for (String s : idsToHide) {
 					String surveyId = s;
 					view.loadUrl("javascript:disableSection('" + surveyId + "');");
 				}
-
 			}
-
-			@Override
-			public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-				handler.proceed();
-			}
-
 		});
-
+		//load webpage from assets
 		mWebView.loadUrl("file:///android_asset/list.html");
 	}
 
